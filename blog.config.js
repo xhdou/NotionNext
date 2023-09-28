ea31f4d328007d4cd2b8b6c7b5aa57d8e2a89aab
xhdou <zhanghao-886@qq.com>
2023-09-28 14:01:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,13 +2,8 @@
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
-<<<<<<< HEAD
       process.env.NOTION_PAGE_ID || '08abd76e5eca432d83267e0f39066086',
   PSEUDO_STATIC: false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
-=======
-        process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5',
-  PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
->>>>>>> main
   NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新内容缓存间隔 单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
   THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
@@ -171,26 +166,13 @@ const BLOG = {
   ],
 
   // 樱花飘落特效
-<<<<<<< HEAD
   SAKURA: process.env.NEXT_PUBLIC_SAKURA || true, // 开关
-
   // 漂浮线段特效
   NEST: process.env.NEXT_PUBLIC_NEST || true, // 开关
-
-=======
-  SAKURA: process.env.NEXT_PUBLIC_SAKURA || false, // 开关
-  // 漂浮线段特效
-  NEST: process.env.NEXT_PUBLIC_NEST || false, // 开关
->>>>>>> main
   // 动态彩带特效
   FLUTTERINGRIBBON: process.env.NEXT_PUBLIC_FLUTTERINGRIBBON || true, // 开关
   // 静态彩带特效
-<<<<<<< HEAD
   RIBBON: process.env.NEXT_PUBLIC_RIBBON || true, // 开关
-
-=======
-  RIBBON: process.env.NEXT_PUBLIC_RIBBON || false, // 开关
->>>>>>> main
   // 星空雨特效 黑夜模式才会生效
   STARRY_SKY: process.env.NEXT_PUBLIC_STARRY_SKY || true, // 开关
 
