ea8a7169be60eb4769a07bcad9076fd06a6f2149
xhdou <zhanghao-886@qq.com>
2026-01-29 01:14:27 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -41,7 +41,7 @@ module.exports = {
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.163.com/song/media/outer/url?id=3342986256.mp3',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'https://p1.music.126.net/Q_fo5pbOcXwtB5fcurQlVQ==/109951172642837003.jpg'
     },
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
