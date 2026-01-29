e1e08537dec26429ce275e699c672e728b01b00c
xhdou <zhanghao-886@qq.com>
2026-01-29 20:41:28 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -43,6 +43,13 @@ module.exports = {
       cover:
         'https://p1.music.126.net/Q_fo5pbOcXwtB5fcurQlVQ==/109951172642837003.jpg'
     },
+    {
+      name: '王都グランセル',
+      artist: 'Falcom Sound Team jdk',
+      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+      cover: 'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    }
+  ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'netease', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
